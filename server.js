f1f29d5459bb870b0dacd7be58bf00c554fda7ab
will.shull@gmail.com <will.shull@gmail.com>
2015-07-29 19:32:58 -0700

    moved words and functions from server file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 'use strict';
 
 var express = require('express');
+var bodyparser = require('body-parser');
+var getRandomWord = require('./lib/getRandomWord.js');
+var postRandomWord = require('./lib/postRandomWord');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var adjectivesModule = require('./lib/adjectives.js');
+var nounsModule = require('./lib/nouns.js');
+var verbsModule = require('./lib/verbs.js');
+var adjectives = adjectivesModule();
+var nouns = nounsModule();
+var verbs = verbsModule();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/app/'));
 
 app.get('/', function(req, res) {
@@ -11,60 +23,56 @@ app.get('/', function(req, res) {
 });
 
 app.get('/adjective', function(req, res) {
-  res.json(getRandomWord(adjective));
+  res.json(getRandomWord(adjectives));
+});
+app.post('/adjective', function(req, res) {
+  console.log(req.body);
+  res.json(postRandomWord(req.body.word, adjectives));
 });
 
 app.get('/noun', function(req, res) {
-  res.json(getRandomWord(noun));
+  res.json(getRandomWord(nouns));
 });
 
 app.get('/verb', function(req, res) {
-  res.json(getRandomWord(verb));
+  res.json(getRandomWord(verbs));
 });
 
 app.listen(port, function() {
   console.log('Server running on port ' + port);
 });
 
-//adjective constructor function
-var Adjectives = function() {
-  this.speedy = true;
-  this.slowly = true;
-  this.shiny = true;
-  this.sleepy = true;
-  this.fabulous = true;
-  this.grandoise = true;
-  this.husky = true;
-  this.mediocre = true;
-  this.nutty = true;
-};
+// //adjective constructor function
+// var Adjectives = function() {
+//   this.speedy = true;
+//   this.slowly = true;
+//   this.shiny = true;
+//   this.sleepy = true;
+//   this.fabulous = true;
+//   this.grandoise = true;
+//   this.husky = true;
+//   this.mediocre = true;
+//   this.nutty = true;
+// };
 
-//noun constructor function
-var Nouns = function() {
-  this.washington = true;
-  this.laptop = true;
-  this.coffee = true;
-  this.platypus = true;
-  this.seattle = true;
-};
+// //noun constructor function
+// var Nouns = function() {
+//   this.washington = true;
+//   this.laptop = true;
+//   this.coffee = true;
+//   this.platypus = true;
+//   this.seattle = true;
+// };
 
-//verb constructor function
-var Verbs = function() {
-  this.run = true;
-  this.jump = true;
-  this.swim = true;
-  this.type = true;
-  this.yawn = true;
-};
+// //verb constructor function
+// var Verbs = function() {
+//   this.run = true;
+//   this.jump = true;
+//   this.swim = true;
+//   this.type = true;
+//   this.yawn = true;
+// };
 
-//word retrieval function
-function getRandomWord(object) {
-  //Put properties into an array, then return a random one
-  var propArray = Object.keys(object);
-  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
-  return {word: randomProp};
-};
-
-var adjective = new Adjectives();
-var noun = new Nouns();
-var verb = new Verbs();
+// var adjective = new Adjectives();
+// var noun = new Nouns();
+// var verb = new Verbs();
